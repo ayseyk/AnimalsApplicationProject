2b71016376d88add588ae8045b36985f953137b7
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-12 15:01:07 +0300

    create AnimalListAdapter.kt

---
+++ app/src/main/java/com/example/animalsapp/view/AnimalListAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.animalsapp.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.animalsapp.R
+import com.example.animalsapp.model.Animal
+import kotlinx.android.synthetic.main.item_animal.view.*
+
+
+class AnimalListAdapter(private val animalList : ArrayList<Animal>) : RecyclerView
+.Adapter<AnimalListAdapter.AnimalViewHolder>() {
+
+    fun updateAnimalList(newAnimalList : List<Animal>){
+        animalList.clear()
+        animalList.addAll(newAnimalList)
+        notifyDataSetChanged() //refresh recyclerView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_animal,parent,false)
+        return AnimalViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = animalList.size
+
+    override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
+        holder.view.animalName.text = animalList[position].name
+    }
+
+    class AnimalViewHolder(var view : View) : RecyclerView.ViewHolder(view)
+}
