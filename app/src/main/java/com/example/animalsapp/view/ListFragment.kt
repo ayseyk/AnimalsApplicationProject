36f206db10c47ffcdaae36e786d30f470f595f5d
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-12 17:17:01 +0300

    update ListFragment.kt and add Observer values

--- app/src/main/java/com/example/animalsapp/view/ListFragment.kt
+++ app/src/main/java/com/example/animalsapp/view/ListFragment.kt
@@ -1,19 +1,44 @@
 package com.example.animalsapp.view
 
 import android.os.Bundle
-import android.text.Layout
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import com.example.animalsapp.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.animalsapp.databinding.FragmentListBinding
+import com.example.animalsapp.model.Animal
+import com.example.animalsapp.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
 
-    lateinit var binding : FragmentListBinding
+    private lateinit var binding : FragmentListBinding
+    private lateinit var viewModel : ListViewModel
+    private val listAdapter = AnimalListAdapter(arrayListOf())
+
+    private val animalListDataObserver = Observer<List<Animal>>{ list ->
+        list?.let{
+            //if the list is not null
+            animalList.visibility = View.VISIBLE
+            listAdapter.updateAnimalList(it)
+        }
+    }
+
+    private val loadingLiveDataObserver = Observer<Boolean> { isLoading ->
+        loadingView.visibility = if(isLoading) View.VISIBLE else View.GONE
+        if(isLoading){
+            listError.visibility = View.GONE
+            animalList.visibility = View.GONE
+        }
+    }
+
+    private val errorLiveDataObserver = Observer<Boolean>{ isError ->
+        listError.visibility = if(isError) View.VISIBLE else View.GONE
+    }
 
     override fun onCreateView( //bundan önce viewla alakalı iş yapılamaz
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +47,21 @@ class ListFragment : Fragment() {
         binding = FragmentListBinding.inflate(layoutInflater)
         return binding.root
         //return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java) //viewModel attaches to fragment
+        viewModel.animals.observe(viewLifecycleOwner, animalListDataObserver)
+        viewModel.loading.observe(viewLifecycleOwner, loadingLiveDataObserver)
+        viewModel.loadError.observe(viewLifecycleOwner, errorLiveDataObserver)
+        viewModel.refresh()
+
+        animalList.apply{
+            layoutManager = GridLayoutManager(context,2)
+            adapter = listAdapter
+        }
     }
 
 
