2221a14d724c38f002851c0387d60bcfcdd6d8fc
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-12 11:17:30 +0300

    Initial commit and create fragment structure

---
+++ app/src/main/java/com/example/animalsapp/view/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.example.animalsapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.animalsapp.R
+import com.example.animalsapp.databinding.FragmentDetailBinding
+
+
+class DetailFragment : Fragment() {
+
+    lateinit var binding : FragmentDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailBinding.inflate(layoutInflater)
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnList.setOnClickListener {
+            val action = DetailFragmentDirections.goList()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/animalsapp/view/ListFragment.kt
@@ -0,0 +1,37 @@
+package com.example.animalsapp.view
+
+import android.os.Bundle
+import android.text.Layout
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.animalsapp.R
+import com.example.animalsapp.databinding.FragmentListBinding
+
+
+class ListFragment : Fragment() {
+
+    lateinit var binding : FragmentListBinding
+
+    override fun onCreateView( //bundan önce viewla alakalı iş yapılamaz
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentListBinding.inflate(layoutInflater)
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDetail.setOnClickListener {
+            val action = ListFragmentDirections.goDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+}
