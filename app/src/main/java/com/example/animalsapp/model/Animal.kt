12855ef60f0e30677a12ff11615a997370c6366b
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-14 09:52:18 +0300

    Animal.kt update and data classes create

--- app/src/main/java/com/example/animalsapp/model/Animal.kt
+++ app/src/main/java/com/example/animalsapp/model/Animal.kt
@@ -1,5 +1,33 @@
 package com.example.animalsapp.model
 
+import com.google.gson.annotations.SerializedName
+
+data class ApiKey(
+    val message : String?,
+    val key : String?
+)
+
 data class Animal(
-    val name : String?
+    val name : String?,
+    val taxonomy : Taxonomy?,
+    val location : String?,
+    val speed : Speed?,
+    val diet : String?,
+
+    @SerializedName("lifespan")
+    val lifeSpan : String?,
+
+    @SerializedName("image")
+    val imageUrl : String?
+)
+
+data class Taxonomy(
+    val kingdom : String?,
+    val order : String?,
+    val family : String?
+)
+
+data class Speed(
+    val metric :String?,
+    val imperial : String?
 )
