d4bc3954393f7cd98ab2eef75644b959ab38f157
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-07-14 11:00:35 +0300

    Create AnimalApi.kt and AnimalApiService.kt

---
+++ app/src/main/java/com/example/animalsapp/model/AnimalApi.kt
@@ -0,0 +1,15 @@
+package com.example.animalsapp.model
+
+import io.reactivex.Single
+import retrofit2.http.Field
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface AnimalApi {
+
+    @GET("getKey")
+    fun getApiKey() : Single<ApiKey>
+
+    @POST("getAnimals")
+    fun getAnimals(@Field("key") key : String) : Single<List<Animal>>
+}
---
+++ app/src/main/java/com/example/animalsapp/model/AnimalApiService.kt
@@ -0,0 +1,27 @@
+package com.example.animalsapp.model
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+class AnimalApiService {
+
+    private val BASE_URL = "https://us-central1-apis-4674e.cloudfunctions.net"
+
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(AnimalApi::class.java)
+
+    fun getApiKey() : Single<ApiKey> {
+        return api.getApiKey()
+    }
+
+    fun getAnimals(key : String) : Single<List<Animal>>{
+        return api.getAnimals(key)
+    }
+
+}
