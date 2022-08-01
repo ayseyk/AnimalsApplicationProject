d42e2031ebccfea2b6d493c6b74522deefb0f81f
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-01 13:09:05 +0300

    Update Animal.kt

--- app/src/main/java/com/example/animalsapp/model/Animal.kt
+++ app/src/main/java/com/example/animalsapp/model/Animal.kt
@@ -33,34 +33,11 @@ data class Taxonomy(
     val family : String?
 ): Parcelable
 
+@Parcelize
 data class Speed(
     val metric :String?,
     val imperial : String?
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
+): Parcelable
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(metric)
-        parcel.writeString(imperial)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Speed> {
-        override fun createFromParcel(parcel: Parcel): Speed {
-            return Speed(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Speed?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
 
 data class AnimalPalette(var color : Int)
